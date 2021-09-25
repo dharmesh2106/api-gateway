b75f1ed7a92fdc66954ccde6a3f877fe3f353a2b
Dharmesh <dharmeshparmar2106@gmail.com>
2021-09-25 13:24:05 +0530

    Hystrix implementation

--- src/main/java/com/rms/ApiGatewayApplication.java
+++ src/main/java/com/rms/ApiGatewayApplication.java
@@ -3,10 +3,11 @@ package com.rms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableCircuitBreaker
+@EnableHystrix
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rms/controller/FallBackMethodController.java
+++ src/main/java/com/rms/controller/FallBackMethodController.java
@@ -1,12 +1,26 @@
 package com.rms.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class FallBackMethodController {
-	
+
+	@RequestMapping(value = "/")
+	@HystrixCommand(commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000") })
+	public String hello() throws InterruptedException {
+		Thread.sleep(3000);
+		return "Welcome Hystrix";
+	}
+
 	@GetMapping("/rmsServiceFallBack")
+//	@HystrixCommand(commandProperties = {
+//			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000") })
 	public String rmsServiceFallBackMethod() {
 		return "RMS Service is taking longer than Expected. Please try again later";
 	}
