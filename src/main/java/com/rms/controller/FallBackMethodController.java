203d0590051a3c6ab34a06fc7c705350c7ab682a
Dharmesh <dharmeshparmar2106@gmail.com>
2021-09-26 09:32:48 +0530

    commit-fallback
    hystrix-fallback

--- src/main/java/com/rms/controller/FallBackMethodController.java
+++ src/main/java/com/rms/controller/FallBackMethodController.java
@@ -1,28 +1,31 @@
 package com.rms.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.rms.utils.CommonValues;
 
 @RestController
 public class FallBackMethodController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FallBackMethodController.class);
 
-	@RequestMapping(value = "/")
-	@HystrixCommand(commandProperties = {
-			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000") })
-	public String hello() throws InterruptedException {
-		Thread.sleep(3000);
-		return "Welcome Hystrix";
-	}
+//	@RequestMapping(value = "/")
+//	@HystrixCommand(commandProperties = {
+//			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000") })
+//	public String hello() throws InterruptedException {
+//		Thread.sleep(3000);
+//		return "Welcome Hystrix";
+//	}
 
 	@GetMapping("/rmsServiceFallBack")
 //	@HystrixCommand(commandProperties = {
 //			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000") })
 	public String rmsServiceFallBackMethod() {
-		return "RMS Service is taking longer than Expected. Please try again later";
+		logger.info("RMS Service FallBack, {}", CommonValues.RMS_SERVICE_FAILD);
+		return CommonValues.RMS_SERVICE_FAILD;
 	}
 
 }
---
+++ src/main/java/com/rms/utils/CommonValues.java
@@ -0,0 +1,8 @@
+
+package com.rms.utils;
+
+
+public interface CommonValues {
+	
+	String RMS_SERVICE_FAILD = "RMS Service is taking longer than Expected. Please try again later.";
+}
